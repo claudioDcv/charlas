53934d3967ad9eaa895facc2bb32003226e20fa6
Claudio Rojas <claudiorojas@Operacioness-MacBook-Pro.local>
2016-11-14 18:24:45 -0300

    adicion logica y vista front, horario complejo 3

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -1535,6 +1535,36 @@ function AdminAct(ServiceUsuario, $location, ServiceHTTP, FactoryLoader) {
 angular.module('sistemaCharlas')
   .controller('AdminAct',AdminAct);
 
+AdminActAcad.$inject = ['ServiceUsuario', '$location', 'ServiceHTTP', 'FactoryLoader', 'ServiceHelpers'];
+
+function AdminActAcad(ServiceUsuario, $location, ServiceHTTP, FactoryLoader, ServiceHelpers) {
+
+    var vm = this;
+    vm.data = {};
+    vm.data.actividad = {};
+
+
+    vm.data.actividad.tipo = "0";
+    vm.data.actividad.modalidad = "0";
+
+    vm.goto = goto;
+
+    function goto(url, id) {
+        $location.path('admin/charlas/' + url + '/' + id);
+    }
+
+
+    function initView() {
+      vm.activa = true;
+    }
+    initView();
+
+
+}
+
+angular.module('sistemaCharlas')
+  .controller('AdminActAcad',AdminActAcad);
+
 AdminActExplorar.$inject = ['ServiceUsuario','$location','ServiceHTTP','FactoryLoader','ServiceHelpers'];
 function AdminActExplorar(ServiceUsuario,$location,ServiceHTTP,FactoryLoader,ServiceHelpers){
       var vm = this;
@@ -1579,36 +1609,6 @@ function AdminActExplorar(ServiceUsuario,$location,ServiceHTTP,FactoryLoader,Ser
 
 angular.module('sistemaCharlas')
   .controller('AdminActExplorar',AdminActExplorar);
-
-AdminActAcad.$inject = ['ServiceUsuario', '$location', 'ServiceHTTP', 'FactoryLoader', 'ServiceHelpers'];
-
-function AdminActAcad(ServiceUsuario, $location, ServiceHTTP, FactoryLoader, ServiceHelpers) {
-
-    var vm = this;
-    vm.data = {};
-    vm.data.actividad = {};
-
-
-    vm.data.actividad.tipo = "0";
-    vm.data.actividad.modalidad = "0";
-
-    vm.goto = goto;
-
-    function goto(url, id) {
-        $location.path('admin/charlas/' + url + '/' + id);
-    }
-
-
-    function initView() {
-      vm.activa = true;
-    }
-    initView();
-
-
-}
-
-angular.module('sistemaCharlas')
-  .controller('AdminActAcad',AdminActAcad);
 
 AdminActExplorarHistorial.$inject = ['ServiceUsuario', '$location', 'ServiceHTTP', 'FactoryLoader', 'ServiceHelpers'];
 
@@ -2590,6 +2590,68 @@ function MonitorIndex(ServiceUsuario){
 
 angular.module('sistemaCharlas')
   .controller('MonitorIndex',MonitorIndex);
+
+AdminActVer.$inject = ['ServiceUsuario', '$location', 'ServiceHTTP', 'FactoryLoader', 'ServiceHelpers','$routeParams','$scope'];
+
+function AdminActVer(ServiceUsuario, $location, ServiceHTTP, ServiceHelpers,$routeParams,$scope) {
+
+    var vm = this;
+    vm.param = {};
+    vm.param.id = $routeParams.id;
+
+    vm.data = {};
+
+
+    vm.goto = goto;
+    function goto(url, id) {
+        $location.path('admin/charlas/' + url + '/' + vm.param);
+    }
+
+    vm.duracionCalculada = 0;
+    vm.minutosAHoras = function(minutos) {
+        return ServiceHelpers.minutosAHoras(minutos);
+    }
+
+
+    vm.actividadGrabar = function(){
+      ServiceHTTP.actividadGrabar('');
+      console.log(vm.data.actividad);
+    }
+
+    function initView() {
+
+      vm.data.actividad = {
+        "id" : '',
+        "duracion": '',
+        "temario": '',
+        "tipo_programa": '',
+        "metodologia": '',
+        "activa": false,
+        "objetivo": '',
+        "modalidad": '',
+        "destinatario": '',
+        "programa": '',
+        "descripcion": '',
+        "nombre": ''
+      };
+
+
+      ServiceHTTP.getActividad(vm.param.id,"Cargando Actividad").then(function(data){
+        console.log(data);
+        console.log(vm.data);
+        vm.data.actividad = data.data.data;
+        ServiceHTTP.cerrarMensaje();
+      },function(err){
+        console.log(err);
+      });
+    }
+    initView();
+
+
+}
+
+angular.module('sistemaCharlas')
+  .controller('AdminActVer',AdminActVer);
 
 CharlaPorCodigo.$inject = ['ServiceUsuario','FactorySearchCharla','$location'];
 function CharlaPorCodigo(ServiceUsuario,FactorySearchCharla,$location){
