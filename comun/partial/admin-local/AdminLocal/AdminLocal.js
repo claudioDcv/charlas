249a716d283f531b79b08826ea61016673dfc687
Maximiliano Ramirez <maximiliano.ramirez@unholster.com>
2016-11-18 18:31:21 -0300

    cambios2

---
+++ comun/partial/admin-local/AdminLocal/AdminLocal.js
@@ -0,0 +1,8 @@
+// AdminLocal.js
+'use strict';
+
+function AdminLocal(){
+  var vm = this;
+      vm.data = {};
+}
+AdminLocal.$inject = [];
---
+++ comun/partial/admin-local/AdminLocal/AdminLocal.module.js
@@ -0,0 +1,3 @@
+// AdminLocal.module.js
+'use strict';
+angular.module('comun').controller('AdminLocal',AdminLocal);
